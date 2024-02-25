b5640d7d8007f8bfe7e4ff035afa2ad8eeed9e3e
BejPastan <s.synaszko@gmail.com>
2024-02-25 16:12:15 +0100

    Update RoomBlueprint.cs

--- Assets/Scripts/RoomBlueprint.cs
+++ Assets/Scripts/RoomBlueprint.cs
@@ -1,7 +1,5 @@
 using System;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class RoomBlueprint : MonoBehaviour
 {
@@ -10,16 +8,64 @@ public class RoomBlueprint : MonoBehaviour
     [SerializeField]
     public GameObject floorPref;
     public RoomPart[] parts = new RoomPart[0];
+    public Grid grid;
     
-    public void createNewBlueprint()
+    /// <summary>
+    /// Creating New Blueprint
+    /// </summary> 
+    public void createNewBlueprint(ref Grid grid)
     {
-        CreateNewPart();
+        this.grid = grid;
     }
 
-    private void CreateNewPart()
+    /// <summary>
+    /// Creating new room part
+    /// </summary>
+    public void CreateNewPart()
     {
         Array.Resize(ref parts, parts.Length + 1);
         parts[parts.Length - 1] = new RoomPart();
+    }
+
+    /// <summary>
+    /// this change bounds of one room part
+    /// </summary>
+    public void ChangeSize(Vector2Int startPos, Vector2Int endPos)
+    {
+
+    }
+
+    private void SetFloors(ref RoomPart part, int id)
+    {
+        Vector2Int size = part.GetSize();
+        Vector2Int gridId;
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int z = 0; z < size.y; z++)
+            {
+                gridId = grid.GetGridId(part.gridShift, new Vector2Int(x, z));
+                foreach (RoomPart room in parts)
+                {
+                    Vector2Int id = room.GetIdByGridId(gridId);
+                }
+                part.elements[x, z] = CreateFloor(gridId);
+            }
+        }
+    }
+
+    private Transform CreateFloor(Vector3 pos)
+    {
+        Vector2Int gridId = grid.GetGridId(pos);
+        grid.gridStates[gridId.x, gridId.y] = GridState.blueprint;
+
+        return Instantiate(floorPref, pos, Quaternion.identity).transform;
+    }
+
+    private Transform CreateFloor(Vector2Int gridId)
+    {
+        Vector3 pos = grid.GetWorldPosition(gridId);
+        grid.gridStates[gridId.x, gridId.y] = GridState.blueprint;
+        return Instantiate(floorPref, pos, Quaternion.identity).transform;
     }
 }
 
@@ -27,6 +73,14 @@ public struct RoomPart
 {
     public Transform[,] elements;
     public Vector2Int gridShift;
+    public Vector2Int gridEnd;
+
+    public RoomPart(Vector2Int gridShift, Vector2Int gridEnd)
+    {
+        this.gridShift = gridShift;
+        this.gridEnd = gridEnd;
+        elements = new Transform[0, 0];
+    }
 
     public Transform GetObjectFromId(int x, int y)
     {
@@ -41,5 +95,15 @@ public struct RoomPart
     public Vector2Int GetIdByGridId(int x, int y)
     {
         return new Vector2Int(x - gridShift.x, y - gridShift.y);
+    }
+
+    public Vector2Int GetGridId(Vector2Int id)
+    {
+        return id-gridShift;
+    }
+
+    public Vector2Int GetSize()
+    {
+        return gridEnd - gridShift;
     }
 }
