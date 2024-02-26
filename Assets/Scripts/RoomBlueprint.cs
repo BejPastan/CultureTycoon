bf1ce0e436e1231c182d204f887517df58c83f3b
BejPastan <s.synaszko@gmail.com>
2024-02-26 23:24:38 +0100

    Update RoomBlueprint.cs

--- Assets/Scripts/RoomBlueprint.cs
+++ Assets/Scripts/RoomBlueprint.cs
@@ -34,12 +34,13 @@ public class RoomBlueprint : MonoBehaviour
     /// </summary>
     public void ChangeSize(Vector2Int startPos, Vector2Int endPos)
     {
+        Debug.Log("ChangeSize");
         ChangeSize(startPos, endPos, ref parts[parts.Length - 1]);
     }
 
     private void ChangeSize(Vector2Int startPos, Vector2Int endPos, ref RoomPart part)
     {
-        
+        ClearPart(ref part);
         part.Resize(startPos, endPos);
         SetFloors(ref part);
     }
@@ -48,10 +49,12 @@ public class RoomBlueprint : MonoBehaviour
     {
         Vector2Int size = part.GetSize();
         Vector2Int gridId;
-        for (int x = 0; x < size.x; x++)
+        Debug.Log("SetFloors");
+        for (int x = 1; x < size.x-1; x++)
         {
-            for (int z = 0; z < size.y; z++)
+            for (int z =1; z < size.y-1; z++)
             {
+                Debug.Log("SetFloors at "+ x + ", "+z);
                 gridId = grid.GetGridId(part.gridShift, new Vector2Int(x, z));
                 bool isFloor = false;
                 foreach (RoomPart room in parts)
@@ -62,7 +65,7 @@ public class RoomBlueprint : MonoBehaviour
                     }
                     else
                     {
-                        //removing other objects i will do it later
+                        RemoveElement(ref part, gridId);
                     }
                 }
                 if(!isFloor)
@@ -82,6 +85,12 @@ public class RoomBlueprint : MonoBehaviour
             grid.ChangeGridState(GridState.blueprint, gridId, gridId);
         }       
         room.CreateElement(room.GetIdByGridId(gridId), ref pref);
+    }
+
+    private void RemoveElement(ref RoomPart part, Vector2Int gridId)
+    {
+        part.RemoveElement(part.GetIdByGridId(gridId));
+        grid.ChangeGridState(GridState.free, gridId);
     }
 
     private void ClearPart(ref RoomPart part)
@@ -132,6 +141,8 @@ public struct RoomPart
         }
         startPos-= Vector2Int.one;
         endPos+= Vector2Int.one;
+        gridShift = startPos;
+        gridEnd = endPos;
         Vector2Int size = endPos - startPos+Vector2Int.one;
 
         Vector2Int deltaShift = startPos - gridShift;
@@ -198,7 +209,8 @@ public struct RoomPart
 
     public Vector2Int GetSize()
     {
-        Vector2Int size = new Vector2Int(gridEnd.x - gridShift.x, gridEnd.y - gridShift.y);
+        Vector2Int size = new Vector2Int(elements.GetLength(0), elements.GetLength(1));
+        Debug.Log("Size: " + size);
         return size;
     }
 }
