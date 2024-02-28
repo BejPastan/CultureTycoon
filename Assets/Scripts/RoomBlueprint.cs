67012e3bee08d7b8fa0565bf5a81187eb634bb58
BejPastan <s.synaszko@gmail.com>
2024-02-28 15:36:53 +0100

    Update RoomBlueprint.cs

--- Assets/Scripts/RoomBlueprint.cs
+++ Assets/Scripts/RoomBlueprint.cs
@@ -1,5 +1,7 @@
 using System;
+using Unity.Burst.Intrinsics;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class RoomBlueprint : MonoBehaviour
@@ -41,8 +43,8 @@ public class RoomBlueprint : MonoBehaviour
     private void ChangeSize(Vector2Int startPos, Vector2Int endPos, ref RoomPart part)
     {
         ClearPart(ref part);
-        part.Resize(startPos, endPos);
-        SetFloors(ref part);
+        part.Resize(startPos, endPos);//kinda git
+        SetFloors(ref part);//kinda git
         for(int roomIndex = 0; roomIndex < parts.Length; roomIndex++)
         {
             
@@ -71,15 +73,16 @@ public class RoomBlueprint : MonoBehaviour
                 int roomIndex = 0;
                 foreach (RoomPart room in parts)
                 {
-                    if(room.GetFloorByGridId(gridId)!= null)
-                        if(room.GetFloorByGridId(gridId).CompareTag("Floor"))
-                        {
-                            isFloor = true;
-                        }
-                        else
-                        {
-                            RemoveElement(roomIndex, gridId);
-                        }
+                    //here i need to check if in this place is floor or walls
+                    if(room.GetFloorByGridId(gridId) != null)
+                    {
+                        isFloor = true;
+                    }
+                    RoomCell[] walls = room.GetWallCellsFromGridId(gridId);
+                    foreach (RoomCell wall in walls)
+                    {
+                        wall.RemoveWallByGridId(gridId);
+                    }
                     roomIndex++;
                 }
                 if(!isFloor)
@@ -133,7 +136,6 @@ public class RoomBlueprint : MonoBehaviour
     private void CreateWall(ref RoomPart part, Vector2Int id, Vector2Int centerId)
     {
         id = part.GetIdByGridId(id);
-        centerId -= id;
         part.CreateWall(id, ref wallPref, centerId);
     }
 
@@ -148,16 +150,19 @@ public class RoomBlueprint : MonoBehaviour
         room.CreateFloor(room.GetIdByGridId(gridId), ref pref);
     }
 
-    private void RemoveElement(ref RoomPart part, Vector2Int gridId)
+    /// <summary>
+    /// Remove all walls that are on this grid cell
+    /// </summary>
+    /// <param name="roomIndex"></param>
+    /// <param name="gridId"></param>
+    private void RemoveWallsOnCell(Vector2Int gridId)
     {
-        part.RemoveElement(part.GetIdByGridId(gridId));
-        grid.ChangeGridState(GridState.free, gridId);
-    }
-
-    private void RemoveElement(int roomIndex, Vector2Int gridId)
-    {
-        Debug.Log("RemoveElement at "+ gridId + " from "+roomIndex);
-        RemoveElement(ref parts[roomIndex], gridId);
+        //Debug.Log("RemoveElement at "+ gridId + " from "+roomIndex);
+        foreach (var room in parts)
+        {
+            //remove walls in position
+        }
+        
     }
 
     private void ClearPart(ref RoomPart part)
@@ -187,8 +192,8 @@ public struct RoomPart
 
     public RoomPart(ref Grid grid)
     {
-        this.gridShift = Vector2Int.zero;
-        this.gridEnd = Vector2Int.zero;
+        gridShift = Vector2Int.zero;
+        gridEnd = Vector2Int.zero;
         elements = new RoomCell[0, 0];
         this.grid = grid;
     }
@@ -208,46 +213,46 @@ public struct RoomPart
             startPos.y = endPos.y;
             endPos.y = temp;
         }
-        startPos-= Vector2Int.one;
-        endPos+= Vector2Int.one;
+
         gridShift = startPos;
         gridEnd = endPos;
         Vector2Int size = endPos - startPos+Vector2Int.one;
 
         Vector2Int deltaShift = startPos - gridShift;
 
-        Transform[,] newElements = new Transform[size.x, size.y];
+        RoomCell[,] newElements = new RoomCell[size.x, size.y];
         for (int x = 0; x < size.x; x++)
         {
             for (int y = 0; y < size.y; y++)
             {
                 if (x + deltaShift.x >= 0 && x + deltaShift.x < elements.GetLength(0) - 1 && y + deltaShift.y >= 0 && y + deltaShift.y < elements.GetLength(1) - 1)
                 {
-                    newElements[x, y] = this.elements[x + deltaShift.x, y + deltaShift.y];
+                    newElements[x, y] = elements[x + deltaShift.x, y + deltaShift.y];
                 }
             }
         }
         elements = newElements;
     }
 
-    public void CreateWall(Vector2Int id, ref GameObject pref, Vector2Int centerShift)
+    public void CreateWall(Vector2Int id, ref GameObject pref, Vector2Int cellId)
     {
-        Vector3 pos = grid.GetWorldPosition(GetGridId(id + centerShift));
+        Vector3 pos = grid.GetWorldPosition(GetGridId(id + cellId));
         //here goes magic stuff with direction
-        Quaternion rotation = Quaternion.Euler(0, (90*centerShift.y*(centerShift.y+1)) + ((centerShift.x*-90)-90), 0);
+        Quaternion rotation = Quaternion.Euler(0, (90*cellId.y*(cellId.y+1)) + ((cellId.x*-90)-90), 0);
         //create element
-        elements[id.x, id.y] = GameObject.Instantiate(pref, pos, rotation).transform;
+
     }
 
     public void CreateFloor(Vector2Int id, ref GameObject pref)
     {
         Vector3 pos = grid.GetWorldPosition(GetGridId(id));
         //create element
-        elements[id.x, id.y] = GameObject.Instantiate(pref, pos, Quaternion.identity).transform;
+        Transform floor = GameObject.Instantiate(pref, pos, Quaternion.identity).transform;
+        elements[id.x, id.y] = new RoomCell(floor, GetGridId(id));
     }
 
     /// <summary>
-    /// This is fore removing whole cell, removing only one element is not implemented yet
+    /// This is fore removing whole cell
     /// </summary>
     /// <param name="id"></param>
     public void RemoveElement(Vector2Int id)
@@ -263,6 +268,43 @@ public struct RoomPart
     public Transform GetFloorByGridId(Vector2Int gridId)
     {
         return elements[gridId.x - gridShift.x, gridId.y - gridShift.y].GetFloor();
+    }
+
+    public RoomCell[] GetWallCellsFromGridId(Vector2Int gridId)
+    {
+        RoomCell[] wallsCells = new RoomCell[0];
+        Vector2Int startSearch;
+        Vector2Int endSearch;
+        startSearch = new Vector2Int(gridId.x - 1, gridId.y - 1);
+        endSearch = new Vector2Int(gridId.x + 1, gridId.y + 1);
+        if(startSearch.x < 0)
+        {
+            startSearch.x = 0;
+        }
+        if(startSearch.y < 0)
+        {
+            startSearch.y = 0;
+        }
+        if(endSearch.x > elements.GetLength(0))
+        {
+            endSearch.x = elements.GetLength(0);
+        }
+        if(endSearch.y > elements.GetLength(1))
+        {
+            endSearch.y = elements.GetLength(1);
+        }
+        for (int x = startSearch.x; x < endSearch.x; x++)
+        {
+            for (int y = startSearch.y; y < endSearch.y; y++)
+            {
+                if (elements[x, y].GetWall(gridId) != null)
+                {
+                    wallsCells[0] = elements[x, y];
+                }
+            }
+        }
+
+        return wallsCells;
     }
 
     public RoomCell GetCellFromId(int x, int y)
@@ -313,10 +355,12 @@ public partial class RoomCell
 {
     public Transform tile;
     Transform[,] walls;
+    Vector2Int gridPos;
 
-    RoomCell(Transform tile)
+    public RoomCell(Transform tile, Vector2Int gridPos)
     {
         this.tile = tile;
+        this.gridPos = gridPos;
         this.walls = new Transform[3,3];
     }
 
@@ -343,9 +387,13 @@ public partial class RoomCell
         }
     }
 
+    public void RemoveWallByGridId(Vector2Int gridId)
+    {
+        RemoveWall(gridId - gridPos);
+    }
+
     private void RemoveWall(Vector2Int id)
     {
-        
         if (walls[id.x, id.y] != null)
         {
             GameObject.Destroy(walls[id.x, id.y].gameObject);
@@ -356,6 +404,12 @@ public partial class RoomCell
     public Transform GetFloor()
     {
         return tile;
+    }
+
+    public Transform GetWall(Vector2Int gridId)
+    {
+        //+1 because from the center of the cell is 1 unit to the edge to -1 x and -1 z
+        return walls[gridId.x-gridPos.x+1, gridId.y-gridPos.x+1];
     }
 
     private Vector2Int CalcWallId(Quaternion rotation)
