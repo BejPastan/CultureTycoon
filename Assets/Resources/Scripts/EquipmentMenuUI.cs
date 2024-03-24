459fa07993637ed789b9a927c6cce64495450588
BejPastan <s.synaszko@gmail.com>
2024-03-24 13:21:48 +0100

    Item placing UI

--- Assets/Resources/Scripts/EquipmentMenuUI.cs
+++ Assets/Resources/Scripts/EquipmentMenuUI.cs
@@ -1,9 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EquipmentMenuUI : MonoBehaviour
 {
-    [SerializeField] FurnitureData[] furnitures;
+    [SerializeField] List<GameObject> furnitures;
     [SerializeField] GameObject buttonPref;
+    [SerializeField] RoomType roomType;    public RoomType GetRoomType(){ return roomType; }
+
+    public void UnlockNewFurniture(GameObject newFurniture)
+    {
+        Debug.Log("UnlockNewFurniture");
+        for(int i = 0; i<furnitures.Count; i++)
+        {
+            if (furnitures[i].GetComponent<FurnitureData>().name == newFurniture.GetComponent<FurnitureData>().name)
+            {
+                break;
+            }
+        }
+        Debug.Log("FurnitureAdded");
+        furnitures.Add(newFurniture);
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Awake");
+        BuildEquipmentMenu();
+    }
+
+    private void OnDisable()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void BuildEquipmentMenu()
+    {
+        FurniturePlacer furniturePlacer = FindObjectOfType<FurniturePlacer>();
+        for(int i = 0; i<furnitures.Count; i++)
+        {
+            GameObject button = Instantiate(buttonPref, transform);
+            Vector3 pos = new Vector3(0, i * 40, 0);
+            button.GetComponent<RectTransform>().anchoredPosition = pos;
+            //add onclick event
+            Debug.Log("i = " + i);
+            GameObject furnitureToPlace = furnitures[i];
+            button.GetComponent<Button>().onClick.AddListener(delegate { furniturePlacer.StartPlacing(furnitureToPlace); });
+            button.GetComponentInChildren<TextMeshProUGUI>().text = furnitures[i].name;
+            //in future add more info from furniture data
+        }
+    }
 }
--- Assets/Resources/Scripts/NPCStory.cs
+++ Assets/Resources/Scripts/NPCStory.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,11 +18,25 @@ public class NPCStory : ScriptableObject
     {
         for(int i = 0; i<storyToUnlocked.Count; i++)
         {
-            if(storyToUnlocked[i].CheckCompletion(needs))
+            if(storyToUnlocked[i].CheckCompletion(needs, out GameObject[] unlocked))
             {
                 unlockedStory.Add(storyToUnlocked[i]);
                 storyToUnlocked.RemoveAt(i);
                 i--;
+                //find all EquipmentMenuUI and unlock new furnitures
+                EquipmentMenuUI[] equipmentMenuUIs = Resources.FindObjectsOfTypeAll<EquipmentMenuUI>();
+                foreach(EquipmentMenuUI equipmentMenuUI in equipmentMenuUIs)
+                {
+                    while(0<unlocked.Length)
+                    {
+                        if(equipmentMenuUI.GetRoomType() == unlocked[unlocked.Length-1].GetComponent<FurnitureData>().destinationType)
+                        {
+                            equipmentMenuUI.UnlockNewFurniture(unlocked[unlocked.Length-1]);
+                            Array.Resize(ref unlocked, unlocked.Length-1);
+                        }
+                    }
+                }
+
             }
         }
     }
@@ -35,11 +48,12 @@ public struct StoryFragment
     //public bool completed;
     public string story;
     [SerializeField] StoryCondition[] conditions;
-    [SerializeField] FurnitureData[] unlockedFurnitures;
+    [SerializeField] GameObject[] unlockedFurnitures;
 
-    public bool CheckCompletion(Needs[] needs)
+    public bool CheckCompletion(Needs[] needs, out GameObject[] unlocked)
     {
         bool completed = true;
+        unlocked = unlockedFurnitures;
         foreach(StoryCondition condition in conditions)
         {
             foreach(Needs need in needs)
@@ -52,6 +66,7 @@ public struct StoryFragment
                         if(!condition.PassComplete())
                         {
                             completed = false;
+                            unlocked = null;
                         }
                     }
                 }
